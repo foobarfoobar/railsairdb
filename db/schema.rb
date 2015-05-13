99c2f9b9a4de367af5f6ec3f0cdf9df0057c413e
Foo Bar <crowley.13@gmx.de>
2015-05-13 11:36:05 +0200

    migration flight

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513080735) do
+ActiveRecord::Schema.define(version: 20150513084447) do
 
   create_table "airports", force: true do |t|
     t.string   "code"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150513080735) do
   create_table "countries", force: true do |t|
     t.string   "code"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "flights", force: true do |t|
+    t.string   "nr"
+    t.datetime "departure_datetime"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
