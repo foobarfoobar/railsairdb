66cd394ebf486af2b3a39c94eb9429e07e5f71c5
Foo Bar <crowley.13@gmx.de>
2015-05-13 10:03:22 +0200

    commented

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ActiveRecord::Base
+  belongs_to :country #zu dieser Klasse/Tabelle besteht die Assoziation
+       # durch belongs_to sind folgende Methoden verfuegbar: country, country=,
+       # build_country, create_country, country.nil?
+       # ueber Airport-Objekt aufrufbar
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,8 @@
 class Country < ActiveRecord::Base
+  has_many :airports, dependent: :restrict_with_error #Klasse mit Fremdschlüssel in der Tabelle
+      # dependent: :destroy, :delete_all, :nullify, :restrict_with_error, :restrict_with_exception
+      # durch has_many sind folgende Methoden verfuegbar:
+      # airports, airports <<, airports.create, airports.build, airports=, airports.find, airports.size,
+      # airports.empty?, airports.delete
+      # ueber Country-Objekt aufrufbar
 end
